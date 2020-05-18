3a400b9f97085cf1376a454d2eca67fb967f7c13
Ammar Elshamy <ammar@corptia.com>
2020-05-18 16:41:55 +0200

    This is where it all begins...

---
+++ Uber/Controller/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  Uber
+//
+//  Created by Ammar Elshamy on 5/18/20.
+//  Copyright © 2020 Ammar Elshamy. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
